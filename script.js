bde63689e20342c7e8be6372fd9dc89c1ebcb3de
DevByAdnan <adufh5073@gmail.com>
2025-02-05 17:28:07 +0300

    Layout functionality

--- script.js
+++ script.js
@@ -32,20 +32,59 @@ function comms(data){
             </div>`;
             if(obj.replies[0] !== undefined){
                 obj.replies.map( objects =>{
-                    content += 
-                    `<div class="replies-container">
-                        <span class="user-date">
-                            <img src="${objects.user.image.png}" alt="">
-                            <p>${objects.user.username}</p>
-                            <p>${objects.createdAt}</p>
-                        </span>
-                        <p class="comment-para">@${objects.replyingTo} ${objects.content}</p>
-                        <div class="vote-container"></div>
-                        <button class="reply-btn">
-                            <img src="images/icon-reply.svg" alt="">
-                            Reply
-                        </button>
-                    </div>`
+                    //if(objects.user.username !== 'juliusomo'){}else{}
+                    if(objects.user.username !== 'juliusomo'){
+                        content += 
+                        `<div class="replies-container">
+                            <span class="user-date">
+                                <img src="${objects.user.image.png}" alt="">
+                                <p>${objects.user.username}</p>
+                                <p>${objects.createdAt}</p>
+                            </span>
+                            <p class="comment-para"><b>@${objects.replyingTo}</b> ${objects.content}</p>
+                            <div class="vote-container">
+                                <button class="incr">
+                                    <img src="images/icon-plus.svg" alt="" >
+                                </button>
+                                <p>${objects.score}</p>
+                                <button class="decr">
+                                    <img src="images/icon-minus.svg" alt="" >
+                                </button>
+                            </div>
+                            <button class="reply-btn">
+                                <img src="images/icon-reply.svg" alt="">
+                                Reply
+                            </button>
+                        </div>`
+                    }else{
+                        content += 
+                        `<div class="replies-container">
+                            <span class="user-date">
+                                <img src="${objects.user.image.png}" alt="">
+                                <p>${objects.user.username}</p>
+                                <p class="you">you</p>
+                                <p>${objects.createdAt}</p>
+                            </span>
+                            <p class="comment-para"><b>@${objects.replyingTo}</b> ${objects.content}</p>
+                            <div class="vote-container">
+                                <button class="incr">
+                                    <img src="images/icon-plus.svg" alt="" >
+                                </button>
+                                <p>${objects.score}</p>
+                                <button class="decr">
+                                    <img src="images/icon-minus.svg" alt="" >
+                                </button>
+                            </div>
+                            <button class="delete-btn">
+                                <img src="images/icon-delete.svg" alt="">
+                                Delete
+                            </button>
+                            <button class="edit-btn">
+                                <img src="images/icon-edit.svg" alt="">
+                                Edit
+                            </button>
+                        </div>`
+                    }
                 })
             }
             
@@ -54,7 +93,7 @@ function comms(data){
     `<div class="current-user">
         <img src="${data.currentUser.image.png}" alt="" class="current-user-img">
         <input class="current-user-input" placeholder = 'Add a comment'></input>
-        <button class="current-user-btn">SEND</button>
+        <input class="current-user-btn" value="SEND" type="submit"></input>
     </div>`
 
     body.innerHTML = content +currentUser;
@@ -63,6 +102,7 @@ function comms(data){
 fetchfiles.then( response =>{
     response.json()
     .then(data =>{
+        datas = data;
         comms(data);
     })
 }) 
